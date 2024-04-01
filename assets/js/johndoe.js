db3ed11dd48127359816650e0868fa3edfafcf6e
Disha Das <44092220+dishadas168@users.noreply.github.com>
2024-04-01 14:21:14 +0000

    j

--- assets/js/johndoe.js
+++ assets/js/johndoe.js
@@ -1,6 +1,6 @@
 /*!
 =========================================================
-* JohnDoe Landing page
+* Disha Das Landing page
 =========================================================
 
 * Copyright: 2019 DevCRUD (https://devcrud.com)
